07c057c8966d769949775a8150906f0388c31196
KevinChicoD <143564440+KevinChicoD@users.noreply.github.com>
2024-05-02 18:52:37 -0600

    Add files via upload

---
+++ EVA3_11_CUSTOM_EXCEPTION/src/eva3_11_custom_exception/EVA3_11_CUSTOM_EXCEPTION.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_11_custom_exception;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_11_CUSTOM_EXCEPTION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try {
+            mensaje();
+        } catch (MiExcepcion e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void mensaje() throws MiExcepcion{
+        System.out.println("mensaje");
+        throw new MiExcepcion("Falla del sistema!!!!");
+    }
+}
+
+class MiExcepcion extends Exception{
+
+    public MiExcepcion() {
+    }
+
+    public MiExcepcion(String Mensaje) {
+        super(Mensaje);
+    }
+    
+}
